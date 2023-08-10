00256c7e2ad1ca60ebc9d1d1d37bbb43090d7f22
ishaan766 <ishaan766@gmail.com>
2023-08-10 12:55:33 +0530

    ch-navbar title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,19 +67,19 @@ const config = {
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: "CubeAPM",
+        title: "CubeAPM Docs",
         logo: {
           alt: "CubeAPM",
           href: '/',
           src: "img/logo.svg",
         },
         items: [
-          {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Docs",
-          },
+          // {
+          //   type: "docSidebar",
+          //   sidebarId: "tutorialSidebar",
+          //   position: "left",
+          //   label: "Docs",
+          // },
           // {
           //   to: '/blog',
           //   label: 'Blog',
